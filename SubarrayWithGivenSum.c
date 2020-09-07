21c007a1559190acc0b510bbd30be397da108792
yashasvi18 <47742064+yashasvi18@users.noreply.github.com>
2020-09-07 18:29:43 +0530

    Create SubarrayWithGivenSum.c

---
+++ SubarrayWithGivenSum.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{   
+    int num,N,S,sum,i,j,l,sub,counter;
+	int a[100];
+    scanf("%d",&num);
+    
+	for(int k=0;k<num;k++)
+	{
+        scanf("%d %d",&N,&S);
+       
+	    for(j=1;j<=N;j++)
+	    {
+	        scanf("%d",&a[j]);
+		}
+	    
+	   	for(i=1;i<=N;i++)
+	    {
+            sum =0;
+	        sub = S - sum;
+            int l = i;
+            counter=0;
+	        while(sub >= a[l])
+	        {
+                sum = sum + a[l];
+	             sub = S-sum;
+                if(sum == S)
+                {
+                    counter ++ ;
+                    printf("%d %d \n",i,l);
+                }
+              
+                else
+                {
+                     l++;
+                }
+            }
+            
+            if(counter ==1)
+            {
+                break;
+            }
+        }
+        if(counter != 1)
+        {
+            printf("-1");
+        }
+    }
+    
+
+}
+	    
+
+	
